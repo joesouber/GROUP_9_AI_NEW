d3e6089f803bc43ec5b14da0f7df0dd615d18749
saffron-j-kendrick <rz20405@bristol.ac.uk>
2023-02-27 08:45:38 +0000

    added a bit on data exploration and created target column

--- car_data.py
+++ car_data.py
@@ -1,3 +1,5 @@
+#%% IMPORTS
+
 import random
 import matplotlib.pyplot as plt
 import numpy as np
@@ -11,32 +13,7 @@ car_data = pd.read_csv(car_data, header = 0, skiprows=0, low_memory=False)
 car_data.head()
 car_data.columns
 
-#%%
-# # Identify the unique car makes in the dataset
-# makes = car_data['make'].unique()
-
-# # Create an empty dictionary to hold the subsets
-# subsets = {i: pd.DataFrame() for i in range(10)}
-
-# # For each make, randomly select a proportionate number of rows to include in each subset
-# for make in makes:
-#     make_df = car_data[car_data['make'] == make]
-#     make_size = len(make_df)
-#     indices = np.arange(make_size)
-#     np.random.shuffle(indices)
-#     for i in range(10):
-#         subset_size = make_size // 10
-#         start = i * subset_size
-#         end = (i + 1) * subset_size
-#         subset_indices = indices[start:end]
-#         subset = make_df.iloc[subset_indices]
-#         subsets[i] = pd.concat([subsets[i], subset])
-
-# # Save each subset to a separate file
-# for i in range(10):
-#     subsets[i].to_csv(f'path/to/subset_{i}.csv', index=False)
-
-#%%
+#%% CREATE SUBSETS FOR TRAINING/TESTING
 # Identify the unique car manufacturers in the dataset
 car_manufacturer = car_data['Manufacturer'].unique()
 
@@ -60,3 +37,17 @@ for make in car_manufacturer:
 # Save each subset to a separate file
 for i in range(10):
     subsets[i].to_csv(f'path/to/subset_{i}.csv', index=False)  # maybe need to remove the 'path/to/' to run locally 
+
+#%% DATA EXPLORATION
+
+car_data.shape   #size of the data frame
+car_data.info()    #columns and the data type
+car_data.isnull().sum()   #any NaN values or empty cells
+car_data.select_dtypes(include=['object']).value_counts()   #checking for unique 
+
+#separate object columns 
+object_columns = car_data.select_dtypes(include=['object'])
+#separate numerical columns
+numeric_columns = car_data.select_dtypes(include=['int', 'float']).drop(['Price'], axis = 1)
+#create a target column - we are training to predict price
+target = car_data.Price
