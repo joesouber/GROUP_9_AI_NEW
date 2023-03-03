13b3493916e4d6dcd91a34d080e4c6647483cec4
joesouber <78925833+joesouber@users.noreply.github.com>
2023-03-03 19:39:48 +0000

    ORGANISING AND ADDING IN GRIDSEARCH

--- car_data.py
+++ car_data.py
@@ -5,38 +5,42 @@ import matplotlib.pyplot as plt
 import numpy as np
 import csv
 import pandas as pd
-#import seaborn as sn
+import seaborn as sn
+from google.colab import drive
 from sklearn.impute import KNNImputer
-#from google.colab import drive
 from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.pipeline import Pipeline
-from sklearn.linear_model import Lasso
-from sklearn.linear_model import Ridge
 from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
 from sklearn import datasets
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.cluster import KMeans
 from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_squared_error as mse
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import r2_score
-import scipy.optimize
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+import time
+drive.mount('/content/drive')
 
-#drive.mount('/content/drive')
+#%%
+# PUT THE CAR_PRICE_PREDICTION FILE IN THE SAME FOLDER AS THE AI FOLDER
+car_data = '/content/drive/MyDrive/AI shared drive/car_price_prediction.csv'
+car_data = pd.read_csv(car_data, header = 0, skiprows=0, low_memory=False)
 
-
-#%% PUT THE CAR_PRICE_PREDICTION FILE IN THE SAME FOLDER AS THE AI FOLDER
-#car_data = '/content/drive/MyDrive/AI shared drive/car_price_prediction.csv'
-car_data = pd.read_csv('car_price_prediction.csv', header = 0, skiprows=0, low_memory=False)
-
+#CLEANING
 
 car_data.replace('-',np.nan, inplace=True) #replaces '-' with Nan values
 car_data['Levy'] = car_data['Levy'].astype('float64')
 
-car_data['Mileage'] = car_data['Mileage'].str.extract('(\d+)').astype(int) # This is going to remove the 'km' in the mileage
-car_data['Mileage'] = car_data['Mileage'].astype('int64')
+car_data['Mileage'] = car_data['Mileage'].str.extract('(\d+)').astype('int64') # This is going to remove the 'km' in the mileage
+#car_data['Mileage'] = car_data['Mileage'].astype('int64')
 
 car_data['Leather interior'] = car_data['Leather interior'].replace({'Yes': True, 'No': False})#replace 'Leather interior yes/no with T/F
 
@@ -48,12 +52,11 @@ car_data['Engine volume'] = car_data['Engine volume'].str.extract(r'(\d+\.\d+|\d
 car_data['Engine volume'] = car_data['Engine volume'].astype('float64')
 car_data['Doors'].replace({'04-May':4, '02-Mar':2, '>5':5}, inplace=True) #replace doors dates with 2,4,5
 
-car_data = car_data.drop('ID', axis=1)
+car_data = car_data.drop('ID', axis=1) #drops a column with not relevant information
 
 
 car_data
 
-#%%
 ## Removes outliars 
 
 def detect_outliers(df, features, threshold=1.5):
@@ -91,23 +94,18 @@ max(car_data_cleaned['Price'])
 
 
 car_data_cleaned
+#%% REMOVING NAN'S IN LEVY COLUMN 
 
+#replace Nan values in levy colum using 
+car_data_cleaned = pd.get_dummies(car_data_cleaned, columns = ['Manufacturer','Model','Category','Fuel type','Gear box type','Drive wheels','Color','Leather interior','Wheel','Turbo']) #one hot encoding the data
 
-#%%
-#attempting to replace Nan values in levy colum using 
-
-#car_data_cleaned.head()
-
-bosh = car_data_cleaned['Levy'].isnull() #finding where the nans are true = Nan, false = no Nan
-nan_indices = car_data_cleaned[bosh].index    #turning into index
+Nanloc = car_data_cleaned['Levy'].isnull() #finding where the nans are true = Nan, false = no Nan
+nan_indices = car_data_cleaned[Nanloc].index    #turning into index
 
 df_with_nans = car_data_cleaned.loc[nan_indices]   #splitting dataset up into nan and no nan
-df_without_nans = car_data_cleaned.loc[~bosh]
-NUMBERS =['Price','Levy','Prod. year', 'Engine volume','Doors', 'Mileage', 'Cylinders', 'Airbags']
-df_without_nans_NUMBERS = df_without_nans[NUMBERS]
-df_with_nans_NUMBERS = df_with_nans[NUMBERS]
+df_without_nans = car_data_cleaned.loc[~Nanloc]
 
-X_train, X_test, y_train, y_test = train_test_split(df_without_nans_NUMBERS.drop('Levy', axis=1), df_without_nans_NUMBERS['Levy'], test_size=0.33, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(df_without_nans.drop('Levy', axis=1), df_without_nans['Levy'], test_size=0.33, random_state=42) #splitting up dataset in test/train and droppping levy as we're predicting this
 
 # create a decision tree regressor object
 reg = DecisionTreeRegressor(random_state=42)
@@ -124,151 +122,94 @@ r2 = r2_score(y_test, y_pred)
 print('We have predicted the levy price with an accuracy of',r2,'on the test set') 
 
 
-#Now we get a r2 score of 78.3% with a test size of 33%. Use this predictor on the Nan dataset.
-df_with_nans_NUMBERS = df_with_nans_NUMBERS.drop('Levy', axis=1)
+#Now we get a r2 score of 98.5.3% with a test size of 33%. Use this predictor on the Nan dataset.
+df_with_nans = df_with_nans.drop('Levy', axis=1)
 
-Nan_pred = reg.predict(df_with_nans_NUMBERS)
+Nan_pred = reg.predict(df_with_nans)
 df_with_nans['Levy'] = Nan_pred
 
 Cleaned_and_final_data = pd.concat([df_with_nans, df_without_nans])
 
-#%%One Hot Encoding
-Cleaned_and_final_data_one_hot = pd.get_dummies(Cleaned_and_final_data, columns = ['Manufacturer','Model','Category','Fuel type','Gear box type','Drive wheels','Color','Leather interior','Wheel','Turbo'])
-X = Cleaned_and_final_data_one_hot.drop('Price',axis=1)
-y = Cleaned_and_final_data_one_hot['Price']
+#%% SPLITTING INTO TEST AND TRAIN
+
+#Cleaned_and_final_data_one_hot = pd.get_dummies(Cleaned_and_final_data, columns = ['Manufacturer','Model','Category','Fuel type','Gear box type','Drive wheels','Color','Leather interior','Wheel','Turbo'])
+#hashed out^^ as used above before predicting levy 
+X = Cleaned_and_final_data.drop('Price',axis=1)
+y = Cleaned_and_final_data['Price']
 X = np.array(X)
 y = np.array(y)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state = 1)
-#%%Building Classifiers
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state = 42)
+#%%GRIDSEARCH TO OPTIMISE HYPERPARAMETERS
 
+# Define the range of hyperparameters to test
+param_grid = {
+    'n_estimators': [50, 100, 200],
+    'max_features': ['auto', 'sqrt'],
+    'max_depth': [10, 20, 30, None]
+}
+
+# Create an instance of Random Forest regressor
+rf = RandomForestRegressor()
+
+# Use GridSearchCV to search over the range of hyperparameters
+grid_search = GridSearchCV(rf, param_grid, cv=5, n_jobs=-1)
+
+# Fit the GridSearchCV object with your training data and labels
+grid_search.fit(X_train, y_train)
+
+# Print out the best hyperparameters and best score
+print(f"Best hyperparameters: {grid_search.best_params_}")
+print(f"Best score: {grid_search.best_score_:.4f}")
+
+# Use the best hyperparameters to fit the model on the entire training set
+rf_best = RandomForestRegressor(**grid_search.best_params_)
+rf_best.fit(X_train, y_train)
+
+# Make predictions on the test set
+#y_pred = rf_best.predict(X_test)
+
+
+
+#%%SUPERVISED LEARNING
 #Linear regression model 
 linear_model = LinearRegression()
 linear_model.fit(X_train, y_train)
 linear_regression_score = linear_model.score(X_test, y_test)
-print('Linear regression model produces an accuracy of',linear_regression_score)
-#%%
-# finding the optimal value of alpha by plotting the r2 score against alpha
-alpha = []
-r2 = []
+print('Linear regression model produces an accuracy of', linear_regression_score)
 
-step = 0.1
-values = [i for i in range(int(0/step), int(10/step))]
-
-for value in values:
-    print(value * step)
-    alpha.append(value*step)
-    ridge_model = Ridge(alpha=value*step)
-    ridge_model.fit(X_train, y_train)
-    r2.append(ridge_model.score(X_test, y_test))
-
-optimal_alpha = alpha[r2.index(max(r2))]
-print('The optimal value of alpha is', optimal_alpha)
-plt.plot(alpha, r2)
-plt.xlabel('Alpha')
-plt.ylabel('R2 score')
-plt.title('R2 score against alpha')
-plt.show()
-
-#%%
 # Ridge Regression Model
-# Need to find the right value of the hyperparameter alpha - doesn't really matter though as they all produce the same accuracy ~ 46%
+# Need to find the right value of the hyperparameter alpha
 alpha = 1
-ridge_regressor = Ridge(optimal_alpha)
+ridge_regressor = Ridge(alpha)
 ridge_regressor.fit(X_train, y_train)
+
 ridge_regressor_score = ridge_regressor.score(X_test, y_test)
-print('Ridge regression model produces an accuracy of',ridge_regressor_score)
-#%%
-#Random forrest lukes method
+print('Ridge regression model produces an accuracy of', ridge_regressor_score)
+
+#Random forest 
 random_model = RandomForestRegressor()
 random_model.fit(X_train, y_train)
 y_pred = random_model.predict(X_test)
-random_forrest_luke_score = r2_score(y_test,y_pred)
-print('Random forrest regression model produces an accuracy of', random_forrest_luke_score)
-#%%
-#Random forrest joes method 
+random_forest_score = r2_score(y_test,y_pred)
+print('Random forrest regression model produces an accuracy of', random_forest_score)
 
-random_model = RandomForestRegressor()
-random_model.fit(X_train, y_train)
-random_forrest_joe_score = random_model.score(X_test, y_test)
-print('Random forrest regression model produces an accuracy of', random_forrest_joe_score)
-#%%
+
 #Decision tree 
 Dtree_model = DecisionTreeRegressor(random_state=42)
 Dtree_model.fit(X_train, y_train)
 y_pred = Dtree_model.predict(X_test)
-decision_tree_score = r2_score(y_test, y_pred)
-print('Decision tree model produces an accuracy of',decision_tree_score)
+score = r2_score(y_test, y_pred)
+print('Decision tree model produces an accuracy of',score)
 
-
-#%%
-# finding the optimal value of alpha by plotting the r2 score against alpha
-alpha = []
-r2 = []
-
-step = 0.1
-values = [i for i in range(int(0.0001/step), int(10/step))]
-
-for value in values:
-    print(value * step)
-    alpha.append(value*step)
-    lasso_model = Lasso(alpha=value*step)
-    lasso_model.fit(X_train, y_train)
-    r2.append(lasso_model.score(X_test, y_test))
-
-optimal_alpha = alpha[r2.index(max(r2))]
-# plotting the r2 score against alpha
-plt.plot(alpha, r2)
-plt.xlabel('Alpha')
-plt.ylabel('R2 score')
-plt.title('R2 score against alpha')
-plt.show()
-
-
-#%%
-# Building a lasso model 
-lasso_model = Lasso(optimal_alpha)
-lasso_model.fit(X_train, y_train)
-# producing r2 score
-lasso_score = lasso_model.score(X_test, y_test)
-print('Lasso model produces an accuracy of',lasso_score)
-
-#%%
-# finding the optimal value of alpha by plotting the r2 score against alpha
-alpha = []
-r2 = []
-
-for i in range(0,10, 0.1):
-    alpha.append(i/100)
-    lasso_model = Lasso(alpha=i/100)
-    lasso_model.fit(X_train, y_train)
-    r2.append(lasso_model.score(X_test, y_test))
-
-
-
-
-#%%
-# Building a gradient boosting model
-gradient_boosting_model = GradientBoostingRegressor()
-gradient_boosting_model.fit(X_train, y_train)
-# producing r2 score
-gradient_boosting_score = gradient_boosting_model.score(X_test, y_test)
-print('Gradient boosting model produces an accuracy of',gradient_boosting_score)
-
-#%%Trying to build DNN
-
-from keras.models import Sequential
-from keras.layers import Dense, Dropout
-import time
-
+#%% DEEP LEARNING
 start_time = time.time()
 
 # Define the number of input features
-input_dim = X_train.shape[1]
-
+input_dims = X_train.shape[1]
 
 # Define the neural network architecture
 model = Sequential()
-model.add(Dense(512, input_dim=input_dim, activation='relu'))
+model.add(Dense(512, input_dim=input_dims, activation='relu'))
 model.add(Dropout(0.2))
 model.add(Dense(256, activation='relu'))
 model.add(Dropout(0.2))
@@ -278,42 +219,18 @@ model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.2))
 model.add(Dense(32, activation='relu'))
 model.add(Dropout(0.2))
-model.add(Dense(1, activation='linear'))
-metrics=['mse', 'mae', 'mape', 'cosine']
+model.add(Dense(1, activation='softmax'))
+
 # Compile the model
 model.compile(loss='categorical_crossentropy', optimizer='adam',metrics=['accuracy','mse','mae','mape'])
 
 # Train the model
-Model = model.fit(X_train, y_train, epochs=100, batch_size=32, validation_data=(X_test, y_test))
+model.fit(X_train, y_train, epochs=20, batch_size=32, validation_data=(X_test, y_test))
+
+#evaluate the model
+model.evaluate(X_test,y_test,verbose = 2)
 
 end_time = time.time()
 elapsed_time = end_time - start_time
 print("Elapsed time: ", elapsed_time, " seconds")
 
-# Plotting Losses 
-plt.figure()
-plt.plot(Model.history["loss"])
-plt.plot(Model.history["val_loss"])
-
-plt.title('Model Loss During Training or Validation')
-plt.ylabel('Training & Validation Losses')
-plt.xlabel('Epoch')
-plt.legend(['Training Loss', 'Validation Loss'])
-
-
-
-# %%
-# Plotting Accuracy
-
-scores = [linear_regression_score, ridge_regressor_score, random_forrest_luke_score, random_forrest_joe_score, decision_tree_score, lasso_score, gradient_boosting_score]
-names = ['Linear Regression', 'Ridge Regression', 'Random Forrest Luke', 'Random Forrest Joe', 'Decision Tree', 'Lasso', 'Gradient Boosting']
-plt.figure()
-plt.bar(names, scores)
-plt.title('Accuracy of different models')
-plt.ylabel('Accuracy')
-plt.xticks(rotation=90)
-plt.xlabel('Model')
-plt.show()
-
-
-# %%
